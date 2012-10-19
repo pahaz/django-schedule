a3d122f7c4d5463d664b069c77f777fc605bc6f4
Brent O'Connor <epicserve@gmail.com>
2012-10-19 13:41:35 -0700

    Improved the load_example_data management command and removed the unnecessary load_sample_data command

--- events/management/commands/load_example_data.py
+++ events/management/commands/load_example_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import NoArgsCommand
+import sys
 
-from django.core.management.color import no_style
 
 class Command(NoArgsCommand):
     help = "Load some sample data into the db"
@@ -11,117 +11,84 @@ class Command(NoArgsCommand):
         from events.models import Event
         from events.models import Rule
 
-        print "checking for existing data ..."
+        print "Checking for existing data ..."
         try:
             cal = Calendar.objects.get(name="Example Calendar")
             print "It looks like you already have loaded this sample data, quitting."
-            import sys
             sys.exit(1)
         except Calendar.DoesNotExist:
-            print "Sample data not found in db."
-            print "Install it..."
+            pass
 
-
-        print "Create Example Calendar ..."
-        cal = Calendar(name="Example Calendar",slug="example")
+        print "Creating Example Calendar ..."
+        cal = Calendar(name="Example Calendar", slug="example")
         cal.save()
-        print "The Example Calendar is created."
-        print "Do we need to install the most common rules?"
+
         try:
             rule = Rule.objects.get(name="Daily")
         except Rule.DoesNotExist:
-            print "Need to install the basic rules"
-            rule = Rule(frequency = "YEARLY", name = "Yearly", description = "will recur once every Year")
+            print "Adding the basic recurring rules ..."
+            rule = Rule(frequency="YEARLY", name="Yearly", description="will recur once every Year")
             rule.save()
-            print "YEARLY recurrence created"
-            rule = Rule(frequency = "MONTHLY", name = "Monthly", description = "will recur once every Month")
+            print "Yearly recurrence created."
+            rule = Rule(frequency="MONTHLY", name="Monthly", description="will recur once every Month")
             rule.save()
-            print "Monthly recurrence created"
-            rule = Rule(frequency = "WEEKLY", name = "Weekly", description = "will recur once every Week")
+            print "Monthly recurrence created."
+            rule = Rule(frequency="WEEKLY", name="Weekly", description="will recur once every Week")
             rule.save()
-            print "Weekly recurrence created"
-            rule = Rule(frequency = "DAILY", name = "Daily", description = "will recur once every Day")
+            print "Weekly recurrence created."
+            rule = Rule(frequency="DAILY", name="Daily", description="will recur once every Day")
             rule.save()
-            print "Daily recurrence created"
-        print "Rules installed."
+            print "Daily recurrence created."
 
-        print "Create some events"
-        rule = Rule.objects.get(frequency="WEEKLY")
-        data = {
-                'title': 'Exercise',
-                'start': datetime.datetime(2008, 11, 3, 8, 0),
-                'end': datetime.datetime(2008, 11, 3, 9, 0),
-                'end_recurring_period' : datetime.datetime(2009, 6, 1, 0, 0),
-                'rule': rule,
-                'calendar': cal
-               }
-        event = Event(**data)
-        event.save()
+        print "Adding some example events ..."
+        WEEKLY_RULE = Rule.objects.get(frequency="WEEKLY")
+        MONTHLY_RULE = Rule.objects.get(frequency="MONTHLY")
+        YEARLY_RULE = Rule.objects.get(frequency="YEARLY")
 
-        data = {
-                'title': 'Exercise',
-                'start': datetime.datetime(2008, 11, 5, 15, 0),
-                'end': datetime.datetime(2008, 11, 5, 16, 30),
-                'end_recurring_period' : datetime.datetime(2009, 6, 1, 0, 0),
-                'rule': rule,
-                'calendar': cal
-               }
-        event = Event(**data)
-        event.save()
+        Event.objects.create(**{
+            'title': 'Exercise',
+            'start': datetime.datetime(2008, 11, 3, 9, 0),
+            'end': datetime.datetime(2008, 11, 3, 10, 0),
+            'rule': WEEKLY_RULE,
+            'calendar': cal
+        })
 
-        data = {
-                'title': 'Exercise',
-                'start': datetime.datetime(2008, 11, 7, 8, 0),
-                'end': datetime.datetime(2008, 11, 7, 9, 30),
-                'end_recurring_period' : datetime.datetime(2009, 6, 1, 0, 0),
-                'rule': rule,
-                'calendar': cal
-               }
-        event = Event(**data)
-        event.save()
+        Event.objects.create(**{
+            'title': 'Exercise',
+            'start': datetime.datetime(2008, 11, 5, 15, 0),
+            'end': datetime.datetime(2008, 11, 5, 16, 30),
+            'rule': WEEKLY_RULE,
+            'calendar': cal
+        })
 
-        rule = Rule.objects.get(frequency="MONTHLY")
-        data = {
+        Event.objects.create(**{
+            'title': 'Exercise',
+            'start': datetime.datetime(2008, 11, 7, 9, 0),
+            'end': datetime.datetime(2008, 11, 7, 10, 0),
+            'rule': WEEKLY_RULE,
+            'calendar': cal
+        })
+
+        Event.objects.create(**{
                 'title': 'Pay Mortgage',
                 'start': datetime.datetime(2008, 11, 1, 14, 0),
                 'end': datetime.datetime(2008, 11, 1, 14, 30),
-                'end_recurring_period' : datetime.datetime(2009, 10, 2, 0, 0),
-                'rule': rule,
+                'rule': MONTHLY_RULE,
                 'calendar': cal
-               }
-        event = Event(**data)
-        event.save()
+        })
 
-        rule = Rule.objects.get(frequency="YEARLY")
-        data = {
-                'title': "Rock's Birthday Party",
-                'start': datetime.datetime(2008, 12, 11, 19, 0),
-                'end': datetime.datetime(2008, 12, 11, 23, 59),
-                'end_recurring_period' : datetime.datetime(2009, 12, 22, 0, 0),
-                'rule': rule,
-                'calendar': cal
-               }
-        event = Event(**data)
-        event.save()
+        Event.objects.create(**{
+            'title': "Rock's Birthday Party",
+            'start': datetime.datetime(2008, 12, 11, 19, 0),
+            'end': datetime.datetime(2008, 12, 11, 23, 59),
+            'rule': YEARLY_RULE,
+            'calendar': cal
+        })
 
-        data = {
-                'title': 'Christmas Party',
-                'start': datetime.datetime(2008, 12, 25, 19, 30),
-                'end': datetime.datetime(2008, 12, 25, 23, 59),
-                'end_recurring_period' : datetime.datetime(2010, 12, 31, 0, 0),
-                'rule': rule,
-                'calendar': cal
-               }
-        event = Event(**data)
-        event.save()
-
-        data = {
-                'title': 'New Pinax site goes live',
-                'start': datetime.datetime(2009, 1, 6, 11, 0),
-                'end': datetime.datetime(2009, 1, 6, 12, 00),
-                'end_recurring_period' : datetime.datetime(2009, 1, 7, 0, 0),
-                'calendar': cal
-               }
-        event = Event(**data)
-        event.save()
-
+        Event.objects.create(**{
+            'title': 'Christmas Party',
+            'start': datetime.datetime(2008, 12, 25, 19, 30),
+            'end': datetime.datetime(2008, 12, 25, 23, 59),
+            'rule': YEARLY_RULE,
+            'calendar': cal
+        })
