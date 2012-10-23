dbb5f796511d091a85a80fa1c67d86578f950e3b
Brent O'Connor <epicserve@gmail.com>
2012-10-23 13:48:06 -0700

    Changed the label and verbose_name for the rule and end_recurring_period fields in order to make it more intuitive for users.

--- events/forms.py
+++ events/forms.py
@@ -1,14 +1,12 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from events.models import Event, Occurrence
-import datetime
-import time
 
 
 class SpanForm(forms.ModelForm):
 
     start = forms.DateTimeField(widget=forms.SplitDateTimeWidget)
-    end = forms.DateTimeField(widget=forms.SplitDateTimeWidget, help_text = _("The end time must be later than start time."))
+    end = forms.DateTimeField(widget=forms.SplitDateTimeWidget, help_text=_("The end time must be later than start time."))
 
     def clean_end(self):
         if self.cleaned_data['end'] <= self.cleaned_data['start']:
@@ -19,16 +17,16 @@ class SpanForm(forms.ModelForm):
 class EventForm(SpanForm):
     def __init__(self, hour24=False, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
-    
-    end_recurring_period = forms.DateTimeField(help_text = _("This date is ignored for one time only events."), required=False)
-    
+
+    end_recurring_period = forms.DateTimeField(label='Ends on', help_text=_("This date is ignored for one time only events."), required=False)
+
     class Meta:
         model = Event
         exclude = ('creator', 'created_on', 'calendar')
-        
+
 
 class OccurrenceForm(SpanForm):
-    
+
     class Meta:
         model = Occurrence
         exclude = ('original_start', 'original_end', 'event', 'cancelled')
@@ -36,7 +34,7 @@ class OccurrenceForm(SpanForm):
 
 class OccurrenceBackendForm(SpanForm):
     """
-        used only for processing data (for ajax methods)
+    used only for processing data (for ajax methods)
     """
 
     start = forms.DateTimeField()
--- events/models/events.py
+++ events/models/events.py
@@ -32,8 +32,8 @@ class Event(models.Model):
     description = models.TextField(_("description"), null=True, blank=True)
     creator = models.ForeignKey(User, null=True, verbose_name=_("creator"))
     created_on = models.DateTimeField(_("created on"), default=datetime.datetime.now)
-    rule = models.ForeignKey(Rule, null=True, blank=True, verbose_name=_("rule"), help_text=_("Select '----' for a one time only event."))
-    end_recurring_period = models.DateTimeField(_("end recurring period"), null=True, blank=True, help_text=_("This date is ignored for one time only events."))
+    rule = models.ForeignKey(Rule, null=True, blank=True, verbose_name=_("Repeats"), help_text=_("Select '----' for a one time only event."))
+    end_recurring_period = models.DateTimeField(_("Ends on"), null=True, blank=True, help_text=_("This date is ignored for one time only events."))
     calendar = models.ForeignKey(Calendar, blank=True)
     objects = EventManager()
 
